d522db21078e7cdb9b387892cd017a6c4db5cbe8
yexiyue <yexiyue666@qq.com>
2024-05-23 15:15:57 +0800

    feat: modify basename

--- web/src/router/index.tsx
+++ web/src/router/index.tsx
@@ -1,63 +1,68 @@
 import { Layout } from "@/components/Layout";
 import { createBrowserRouter } from "react-router-dom";
 
-export const router = createBrowserRouter([
+export const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          index: true,
+          lazy: () => import("@/pages/Home"),
+        },
+        {
+          path: "template/:id",
+          lazy: () => import("@/pages/templates"),
+        },
+        {
+          path: "tag/:id",
+          lazy: () => import("@/pages/tags"),
+        },
+        {
+          path: "user/:id",
+          lazy: () => import("@/pages/users"),
+        },
+        {
+          path: "user-center",
+          lazy: () => import("@/pages/center"),
+          children: [
+            {
+              index: true,
+              path: "favorite",
+              lazy: () => import("@/pages/center/Favorite"),
+            },
+            {
+              path: "mine",
+              lazy: () => import("@/pages/center/Mine"),
+            },
+            {
+              path: "admin",
+              children: [
+                {
+                  path: "category",
+                  lazy: () => import("@/pages/center/admin/Category"),
+                },
+                {
+                  path: "tag",
+                  lazy: () => import("@/pages/center/admin/Tag"),
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+    {
+      path: "/login",
+      lazy: () => import("@/pages/Login"),
+    },
+    {
+      path: "/auth",
+      lazy: () => import("@/pages/Authorized"),
+    },
+  ],
   {
-    path: "/",
-    element: <Layout />,
-    children: [
-      {
-        index: true,
-        lazy: () => import("@/pages/Home"),
-      },
-      {
-        path: "template/:id",
-        lazy: () => import("@/pages/templates"),
-      },
-      {
-        path: "tag/:id",
-        lazy: () => import("@/pages/tags"),
-      },
-      {
-        path: "user/:id",
-        lazy: () => import("@/pages/users"),
-      },
-      {
-        path: "user-center",
-        lazy: () => import("@/pages/center"),
-        children: [
-          {
-            index: true,
-            path: "favorite",
-            lazy: () => import("@/pages/center/Favorite"),
-          },
-          {
-            path: "mine",
-            lazy: () => import("@/pages/center/Mine"),
-          },
-          {
-            path: "admin",
-            children: [
-              {
-                path: "category",
-                lazy: () => import("@/pages/center/admin/Category"),
-              },
-              {
-                path: "tag",
-                lazy: () => import("@/pages/center/admin/Tag"),
-              },
-            ],
-          },
-        ],
-      },
-    ],
-  },
-  {
-    path: "/login",
-    lazy: () => import("@/pages/Login"),
-  },
-  {
-    path: "/auth",
-    lazy: () => import("@/pages/Authorized"),
-  },
-]);
+    basename: "/actions-workflows",
+  }
+);
--- web/vite.config.ts
+++ web/vite.config.ts
@@ -15,6 +15,7 @@ export default defineConfig({
       "@": "/src",
     },
   },
+  base: "/actions-workflows",
   // server: {
   //   proxy: {
   //     "/api": {
